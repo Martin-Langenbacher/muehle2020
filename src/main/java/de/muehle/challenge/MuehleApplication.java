876aeace5c433e31c959c00f36a83e49e454b1b1
Martin Langenbacher <martin.langenbacher@googlemail.com>
2020-02-20 17:43:33 +0100

    muehle challenge started TWO

---
+++ src/main/java/de/muehle/challenge/MuehleApplication.java
@@ -0,0 +1,13 @@
+package de.muehle.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuehleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuehleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/muehle/challenge/MuehleApplicationTests.java
@@ -0,0 +1,13 @@
+package de.muehle.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuehleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
